2a783ac6e9efe481c189c62a43d40bb2f51cef33
Pablo Arak <pablo.arak.vm@gmail.com>
2021-06-12 18:45:44 -0400

    Added carousel item component

---
+++ src/components/CarouselItem.jsx
@@ -0,0 +1,24 @@
+// Libraries
+import React from 'react';
+import '../assets/styles/components/CarouselItem.scss'
+// Images
+import playIcon from '../assets/icons/play.png';
+import sumIcon from '../assets/icons/sum.png';
+import exampleImage from '../assets/images/mandarina.jpg';
+
+const CarouselItem = ({children}) => (
+    <div className="carousel-item">
+        <img className="carousel-item__img" src={exampleImage} alt="Mandarinas" />
+        <div className="carousel-item__details">
+            <div>
+                <img className="carousel-item__details--img" src={playIcon} alt="Play" />
+                <img className="carousel-item__details--img" src={sumIcon} alt="Plus" />
+            </div>
+            <p className="carousel-item__details--title">Titulo descriptivo</p>
+            <p className="carousel-item__details--subtitle">2020 16+ 114 minutos</p>
+        </div>
+    </div>
+
+);
+
+export default CarouselItem;
