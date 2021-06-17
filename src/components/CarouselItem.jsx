5fab9fba14752faa988fc9f9b65b776f7d55c1c6
Pablo Arak <pablo.arak.vm@gmail.com>
2021-06-17 13:50:54 -0400

    Implement properties in HTML

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -6,16 +6,18 @@ import playIcon from '../assets/static/play.png';
 import sumIcon from '../assets/static/sum.png';
 import exampleImage from '../assets/static/mandarina.jpg';
 
-const CarouselItem = ({children}) => (
+const CarouselItem = ({cover,title,year,contentRating,duration}) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src={exampleImage} alt="Mandarinas" />
+        <img className="carousel-item__img" src={cover} alt={title} />
         <div className="carousel-item__details">
             <div>
                 <img className="carousel-item__details--img" src={playIcon} alt="Play" />
                 <img className="carousel-item__details--img" src={sumIcon} alt="Plus" />
             </div>
-            <p className="carousel-item__details--title">Titulo descriptivo</p>
-            <p className="carousel-item__details--subtitle">2020 16+ 114 minutos</p>
+            <p className="carousel-item__details--title">{title}</p>
+            <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`}
+            </p>
         </div>
     </div>
 
