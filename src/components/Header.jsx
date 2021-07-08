cc19a9b4a69cffe3e181c5874fb6cb84649b58ed
Pablo Arak <pablo.arak.vm@gmail.com>
2021-07-08 18:09:46 -0400

    Added links to anothers components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 // Libraries
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../assets/styles/components/Header.scss'
 // Images
 import logo from '../assets/static/logo.png';
@@ -7,7 +8,10 @@ import userProfile from '../assets/static/user.png';
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src={logo} alt="Platzi Video"/>
+        <Link to="/">
+          <img className="header__img" src={logo} alt="Libre Video"/>
+        </Link>
+        
         <div className="header__menu">
           <div className="header__menu--profile">
             <img src={userProfile} alt="User"/>
@@ -15,7 +19,11 @@ const Header = () => (
           </div>
           <ul>
             <li><a href="/">Cuenta</a></li>
-            <li><a href="/">Cerrar Sesión</a></li>
+            <li>
+              <Link to="/login">
+                Iniciar Sesión
+              </Link>
+            </li>
           </ul>
         </div>
     </header>
