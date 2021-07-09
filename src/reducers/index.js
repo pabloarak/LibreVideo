71cee8bee177b9fc756b70eb62689233e23bd3e6
Pablo Arak <pablo.arak.vm@gmail.com>
2021-07-09 16:45:34 -0400

    Added new reducer for delete videos of my list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,17 @@
+import { actions } from '../actions/index';
+
 const reducer = (state, action) => {
     
     switch (action.type) {
-        case 'SET_FAVORITE':
+        case actions.setFavorite:
             return {
                 ...state,
                 myList: [...state.myList, action.payload]
+            }
+        case actions.deleteFavorite:
+            return {
+                ...state,
+                myList: state.myList.filter(items => items.id !== action.payload)
             }
         default:
             return state;
