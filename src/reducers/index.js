3758c48912c063b7e27aca7ee2d8806b6dd4f30b
Pablo Arak <pablo.arak.vm@gmail.com>
2021-07-09 18:28:12 -0400

    Fixed bug of adding a favorite multiple times to the list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,11 @@ const reducer = (state, action) => {
     
     switch (action.type) {
         case actions.setFavorite:
+            const exist = state.myList.find(item => item.id === action.payload.id);
+
+            if (exist) 
+                return {...state}
+
             return {
                 ...state,
                 myList: [...state.myList, action.payload]
