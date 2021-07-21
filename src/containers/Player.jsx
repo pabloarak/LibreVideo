ec6dc6fc849f501df44f9e97c4555bd04c017dbc
Pablo Arak <pablo.arak.vm@gmail.com>
2021-07-21 13:40:23 -0400

    Added player container

---
+++ src/containers/Player.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { getVideoSource } from '../actions';
+import '../assets/styles/components/Player.scss';
+
+const Player = props => {
+
+    const { id } = props.match.params;
+    const [loading, setLoading] = useState(true);
+    const hasPlaying = Object.keys(props.playing).length > 0;
+
+    useEffect(() => {
+        props.getVideoSource(id);
+        setLoading(false);
+    },[]);
+
+    if (loading) {
+        return <h2>Cargando...</h2>;
+    }
+
+    return hasPlaying ? (
+        <div className="Player">
+            <video controls autoPlay>
+                <source src={props.playing.source} type="video/mp4"/>
+            </video>
+            <div className="Player-back">
+                <button type="button" onClick={() => props.history.goBack()}>
+                    Regresar
+                </button>
+            </div>
+        </div>
+    ) : <Redirect to='/404/'/>;
+};
+
+const mapStateToProps = state => {
+    return {
+        playing: state.playing,
+    }
+}
+
+const mapDispatchToProps = {
+    getVideoSource,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
