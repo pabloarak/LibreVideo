1a8cffc945dcd30207c8ceb9315a2ea036436ad2
Pablo Arak <pablo.arak.vm@gmail.com>
2021-08-19 13:21:52 -0400

    Added initial files for SSR

---
+++ src/server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { ENV, PORT } = process.env;
+const app = express();
+
+if(ENV === 'development') {
+    console.log('Development config');
+}
+
+app.get('*', (req,res) => {
+    res.send({hello: 'express'});
+});
+
+app.listen(PORT, (err) => {
+    if(err) 
+        console.log(err);
+    else
+        console.log('Server running on port '+PORT);
+});
