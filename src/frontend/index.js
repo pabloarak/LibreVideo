c38c31c8a2c7d2380d1c4d2fc79f4072eb7f5092
Pablo Arak <pablo.arak.vm@gmail.com>
2021-10-04 17:32:54 -0300

    Update

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -8,10 +8,10 @@ import { createBrowserHistory } from 'history';
 import reducer from './reducers';
 import App from './routes/App';
 
-const history = createBrowserHistory();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const preloadedState = window.__PRELOADED_STATE__;
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose;
 const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(thunk)));
+const history = createBrowserHistory();
 
 delete window.__PRELOADED_STATE__;
 
