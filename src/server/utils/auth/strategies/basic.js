072e224a385bd05952c0e667cc0df90cb305142d
Pablo Arak <pablo.arak.vm@gmail.com>
2021-10-09 16:16:27 -0300

    Get set-cookie

--- src/server/utils/auth/strategies/basic.js
+++ src/server/utils/auth/strategies/basic.js
@@ -6,27 +6,33 @@ const axios = require('axios');
 require('dotenv').config();
 
 passport.use(
-    new BasicStrategy(async (email, password, cb) => {
-        try {
-            const { data, status } = await axios({
-                url: `${process.env.API_URL}/auth/sign-in`,
-                method: 'post',
-                auth: {
-                    password,
-                    username: email,
-                },
-                data: {
-                    apiKeyToken: process.env.API_KEY_TOKEN,
-                },
-            });
+  new BasicStrategy(async (email, password, cb) => {
+    try {
+      const { data, status, headers } = await axios({
+        url: `${process.env.API_URL}/auth/sign-in`,
+        method: 'post',
+        auth: {
+          password,
+          username: email,
+        },
+        data: {
+          apiKeyToken: process.env.API_KEY_TOKEN,
+        },
+      });
 
-            if(!data || status !== 200) {
-                return cb(boom.unauthorized(), false);
-            }
+      const cookieElements = headers['set-cookie'][0].split('; ');
+      const token = cookieElements.filter((e) => e.includes('token'))[0].split('=')[1];
+      const expires = cookieElements.filter((e) => e.includes('Expires'))[0].split('=')[1];
+      const maxAge = cookieElements.filter((e) => e.includes('Max-Age'))[0].split('=')[1];
+      const path = cookieElements.filter((e) => e.includes('Path'))[0].split('=')[1];
 
-            return cb(null, data);
-        } catch (error) {
-            cb(error);
-        }
-    })
-)
+      if (!data || status !== 200) {
+        return cb(boom.unauthorized(), false);
+      }
+
+      return cb(null, { token, expires, maxAge, path, ...data });
+    } catch (error) {
+      cb(error);
+    }
+  }),
+);
